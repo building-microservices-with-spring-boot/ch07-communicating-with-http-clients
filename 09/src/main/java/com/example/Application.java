8d9b29c25ddd11bba57a388a1039c58b4de5043a
Engin Yöyen <engin.yoyen@gmail.com>
2016-09-16 22:25:32 +0200

    Fix typo

--- 09/src/main/java/com/example/Application.java
+++ 09/src/main/java/com/example/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example")
+@ComponentScan()
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
