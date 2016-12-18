d7e291dbede5a73f838a69c2448bd1e06c6b5980
Engin Yöyen <engin.yoyen@gmail.com>
2016-12-18 22:20:46 +0100

    Fix : Exception handler class type

--- 03/src/main/java/com/example/controller/ExceptionHandlerAdvice.java
+++ 03/src/main/java/com/example/controller/ExceptionHandlerAdvice.java
@@ -4,12 +4,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 
 @ControllerAdvice
 class ExceptionHandlerAdvice {
 
-    @ExceptionHandler(value = HttpServerErrorException.class)
+    @ExceptionHandler(value = HttpClientErrorException.class)
     ResponseEntity<?> handleHttpClientException(HttpClientErrorException exception) throws Exception {
         return new ResponseEntity(exception.getResponseBodyAsByteArray(),
                 exception.getResponseHeaders(),
